1f6b798063bf810e5077c496a7dd8092dd6ed45f
developerprince <N01416165H@students.nust.ac.zw>
2018-10-12 09:41:31 +0200

    agruments parsing

--- Concrete_Comp_Test_Beta.py
+++ Concrete_Comp_Test_Beta.py
@@ -5,17 +5,23 @@ import numpy as np
 import os
 os.getcwd()
 os.listdir(os.getcwd())
+import sys
 import CCST_ML.create_dataset as csdata
 import CCST_ML.generate_model as cs
 
-DT = csdata.RESC_DATA()
+def main():
 
-test_data = DT.__obtain_testing_data__('sample-data/ccs_data.csv')
-train_data = DT.__obtain_training_data__('sample-data/ccs_data.csv')
+    DT = csdata.RESC_DATA()
 
-MODEL = cs.GENERATE_MODEL()
-model = MODEL.__generate__(train_data)
+    test_data = DT.__obtain_testing_data__('sample-data/ccs_data.csv')
+    train_data = DT.__obtain_training_data__('sample-data/ccs_data.csv')
+    input_data = [sys.argv[1], sys.argv[2],sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8]]
+    MODEL = cs.GENERATE_MODEL()
+    model = MODEL.__generate__(train_data)
 
-test_predictions = model.predict(x=test_data.values).flatten()
+    test_predictions = model.predict(x=input_data).flatten()
 
-print(test_predictions)
+    print(test_predictions)
+
+if __name__ == '__main__':
+    main()
